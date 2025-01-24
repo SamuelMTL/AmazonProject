299c83a667ac82bdad2b5b190b58b281fd673815
SamuelMTL <samuel.mtl11@gmail.com>
2025-01-24 16:14:18 -0300

    camera atualizada

--- amazonproject/scripts/dungeon_generator.gd
+++ amazonproject/scripts/dungeon_generator.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var dungeon_size = Vector2(5, 5)
 var dungeon_layout = [] #isso aqui vai armazenar o layout criado
+var max_rooms = 10
+var directions = [Vector2.UP, Vector2.DOWN, Vector2.LEFT, Vector2.RIGHT]
 
 var room_scene = preload("res://scenes/dungeon_room_1.tscn")
 
@@ -10,6 +12,7 @@ func _ready():
 	build_dungeon()
 	
 func generate_dungeon():
+
 	for row in range(dungeon_size.y):
 		var row_data = []
 		for col in range(dungeon_size.x):
@@ -20,8 +23,7 @@ func generate_dungeon():
 	
 	# garantindo a entrada da dungeon no lugar q eu quero
 	dungeon_layout[4][2] = 1 
-	
-	
+
 func build_dungeon():
 	for row in range(dungeon_size.y):
 		for col in range(dungeon_size.x):
@@ -38,12 +40,15 @@ func build_dungeon():
 				var has_bottom = y < dungeon_size.y - 1 and dungeon_layout[y + 1][x] == 1
 				var has_left = x > 0 and dungeon_layout[y][x - 1] == 1
 				var has_right = x < dungeon_size.x - 1 and dungeon_layout[y][x + 1] == 1
-			
-				
+
 				room.get_node("FireWallCorners").play("fire_corners")
-		
+				#garante que a porta da entrada esteja aberta
+				if row == 4 and col == 2:
+					room.get_node("FireWallBottom").play("bottomWall_open")
+				else:
+					room.get_node("FireWallBottom").play("bottomWall_open" if has_bottom else "bottomWall_closed")
+					
 				room.get_node("FireWallTop").play("topWall_open" if has_top else "topWall_closed")
-				room.get_node("FireWallBottom").play("bottomWall_open" if has_bottom else "bottomWall_closed")
 				room.get_node("FireWallLeft").play("leftWall_open" if has_left else "leftWall_closed")
 				room.get_node("FireWallRight").play("rightWall_open" if has_right else "rightWall_closed")
 				
