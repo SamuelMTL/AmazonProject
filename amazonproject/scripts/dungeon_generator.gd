a73ae5a58f10d6917276efdde1fac14edfbafd3c
SamuelMTL <samuel.mtl11@gmail.com>
2025-01-25 14:41:55 -0300

    fiz a primeira sala da dungeon ser diferente das outras

--- amazonproject/scripts/dungeon_generator.gd
+++ amazonproject/scripts/dungeon_generator.gd
@@ -2,10 +2,10 @@ extends Node2D
 
 var dungeon_size = Vector2(5, 5)
 var dungeon_layout = [] #isso aqui vai armazenar o layout criado
-var max_rooms = 10
 var directions = [Vector2.UP, Vector2.DOWN, Vector2.LEFT, Vector2.RIGHT]
 
 var room_scene = preload("res://scenes/dungeon_room_1.tscn")
+var first_room_scene = preload("res://scenes/dungeon_room_first.tscn")
 
 func _ready():
 	generate_dungeon()
@@ -22,34 +22,38 @@ func generate_dungeon():
 		print(row_data)
 	
 	# garantindo a entrada da dungeon no lugar q eu quero
-	dungeon_layout[4][2] = 1 
+	dungeon_layout[4][2] = 2 
 
 func build_dungeon():
 	for row in range(dungeon_size.y):
 		for col in range(dungeon_size.x):
-			if dungeon_layout[row][col] == 1:
-				var room = room_scene.instantiate()
-				add_child(room)
-				room.position = Vector2(col * 640, row * 360)
+			if dungeon_layout[row][col] > 0:
+				var is_first_room = dungeon_layout[row][col] == 2
+				build_room(col, row, is_first_room)
 				
-				var x = col
-				var y = row
-				
-				#verifica se tem sala do lado
-				var has_top = y > 0 and dungeon_layout[y - 1][x] == 1
-				var has_bottom = y < dungeon_size.y - 1 and dungeon_layout[y + 1][x] == 1
-				var has_left = x > 0 and dungeon_layout[y][x - 1] == 1
-				var has_right = x < dungeon_size.x - 1 and dungeon_layout[y][x + 1] == 1
 
-				room.get_node("FireWallCorners").play("fire_corners")
-				#garante que a porta da entrada esteja aberta
-				if row == 4 and col == 2:
-					room.get_node("FireWallBottom").play("bottomWall_open")
-				else:
-					room.get_node("FireWallBottom").play("bottomWall_open" if has_bottom else "bottomWall_closed")
-					
-				room.get_node("FireWallTop").play("topWall_open" if has_top else "topWall_closed")
-				room.get_node("FireWallLeft").play("leftWall_open" if has_left else "leftWall_closed")
-				room.get_node("FireWallRight").play("rightWall_open" if has_right else "rightWall_closed")
-				
-				
+
+func build_room(x, y, is_first_room=false):
+	var room = first_room_scene.instantiate() if is_first_room else room_scene.instantiate()
+	add_child(room)
+	room.position = Vector2(x * 640, y * 360)
+	
+	var has_top = y > 0 and dungeon_layout[y - 1][x] > 0
+	var has_bottom = y < dungeon_size.y - 1 and dungeon_layout[y + 1][x] > 0
+	var has_left = x > 0 and dungeon_layout[y][x - 1] > 0
+	var has_right = x < dungeon_size.x - 1 and dungeon_layout[y][x + 1] > 0
+	
+	room.get_node("FireWallCorners").play("fire_corners")
+	
+	if is_first_room:
+		# Configuração específica para a primeira sala
+		room.get_node("FireWallBottom").play("bottomWall_open")
+		room.get_node("FireWallTop").play("topWall_open" if has_top else "topWall_closed")
+		room.get_node("FireWallLeft").play("leftWall_open" if has_left else "leftWall_closed")
+		room.get_node("FireWallRight").play("rightWall_open" if has_right else "rightWall_closed")
+	else:
+		# Configura portas normais
+		room.get_node("FireWallBottom").play("bottomWall_open" if has_bottom else "bottomWall_closed")
+		room.get_node("FireWallTop").play("topWall_open" if has_top else "topWall_closed")
+		room.get_node("FireWallLeft").play("leftWall_open" if has_left else "leftWall_closed")
+		room.get_node("FireWallRight").play("rightWall_open" if has_right else "rightWall_closed")
