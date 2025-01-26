8942bd796266ab641bd8785b02559ce69f028c29
SamuelMTL <samuel.mtl11@gmail.com>
2025-01-26 16:33:28 -0300

    novas direções do player adicionados (visual)

--- amazonproject/scripts/player.gd
+++ amazonproject/scripts/player.gd
@@ -1,7 +1,28 @@
 extends CharacterBody2D
 
+@onready var animations = $AnimatedSprite2D
+
+const SPEED = 200
+
 func _physics_process(delta):
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = direction * 200
+	handle_input()
+	update_animation()
 	move_and_slide()
 	
+func handle_input():
+	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = moveDirection * SPEED
+	
+func update_animation():
+	var direction 
+	
+	if velocity.x < 0: 
+		direction = "Left"
+	elif velocity.x > 0:
+		direction = "Right"
+	elif velocity.y < 0:
+		direction = "Up"
+	else: 
+		direction = "Down"
+		
+	animations.play(direction + "View")
