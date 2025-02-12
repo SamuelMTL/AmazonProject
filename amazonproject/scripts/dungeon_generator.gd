d8871aff35babcfe3094c450a381eb23afe4620a
SamuelMTL <samuel.mtl11@gmail.com>
2025-02-11 23:59:30 -0300

    build rooms
    começando a definir qual sala ser gerada com base em qual lado há uma porta

--- amazonproject/scripts/dungeon_generator.gd
+++ amazonproject/scripts/dungeon_generator.gd
@@ -82,7 +82,9 @@ func build_room(x, y, is_first_room=false):
 	var has_left = x > 0 and dungeon_layout[y][x - 1] > 0
 	var has_right = x < dungeon_size.x - 1 and dungeon_layout[y][x + 1] > 0
 	
-	var room_scene = first_room_scene if is_first_room else [room1_scene, room2_scene, room3_scene][randi() % 3]
+	var num_connections = int(has_top) + int(has_bottom) + int(has_left) + int(has_right)
+	var room_scene = first_room_scene if is_first_room else get_room_by_connections(num_connections)
+	
 	var room = room_scene.instantiate()
 	add_child(room)
 	room.position = Vector2(x * 640, y * 360)
@@ -95,6 +97,7 @@ func build_room(x, y, is_first_room=false):
 	room.get_node("FireWallLeft/StaticBody2D/WallLeftCollision").disabled = has_left
 	room.get_node("FireWallLeft/StaticBody2D/DoorLeftCollision").disabled = not has_left
 	room.get_node("FireWallLeft/StaticBody2D/DoorLeftCollision2").disabled = not has_left
+	
 	room.get_node("FireWallRight/StaticBody2D/WallRightCollision").disabled = has_right
 	room.get_node("FireWallRight/StaticBody2D/DoorRightCollision").disabled = not has_right
 	room.get_node("FireWallRight/StaticBody2D/DoorRightCollision2").disabled = not has_right
@@ -110,15 +113,21 @@ func build_room(x, y, is_first_room=false):
 		room.get_node("FireWallBottom/StaticBody2D/DoorBottomCollision2").disabled = false
 		# Configuração específica para a primeira sala
 		room.get_node("FireWallBottom").play("bottomWall_open")
-		room.get_node("FireWallTop").play("topWall_open" if has_top else "topWall_closed")
-		room.get_node("FireWallLeft").play("leftWall_open" if has_left else "leftWall_closed")
-		room.get_node("FireWallRight").play("rightWall_open" if has_right else "rightWall_closed")
 	else:
 		room.get_node("FireWallBottom/StaticBody2D/WallBottomCollision").disabled = has_bottom
 		room.get_node("FireWallBottom/StaticBody2D/DoorBottomCollision").disabled = not has_bottom
 		room.get_node("FireWallBottom/StaticBody2D/DoorBottomCollision2").disabled = not has_bottom
 		# Configura portas normais
 		room.get_node("FireWallBottom").play("bottomWall_open" if has_bottom else "bottomWall_closed")
-		room.get_node("FireWallTop").play("topWall_open" if has_top else "topWall_closed")
-		room.get_node("FireWallLeft").play("leftWall_open" if has_left else "leftWall_closed")
-		room.get_node("FireWallRight").play("rightWall_open" if has_right else "rightWall_closed")
+		
+	room.get_node("FireWallTop").play("topWall_open" if has_top else "topWall_closed")
+	room.get_node("FireWallLeft").play("leftWall_open" if has_left else "leftWall_closed")
+	room.get_node("FireWallRight").play("rightWall_open" if has_right else "rightWall_closed")
+
+func get_room_by_connections(num_connections):
+	match num_connections:
+		4: return [room1_scene, room2_scene, room3_scene][randi() % 3]
+		3: return [room4_scene, room5_scene, room6_scene, room7_scene][randi() % 4]
+		2: return [room8_scene, room9_scene, room10_scene, room11_scene, room12_scene, room13_scene][randi() % 6]
+		1: return [room14_scene, room15_scene, room16_scene, room17_scene][randi() % 4]
+	return room1_scene
