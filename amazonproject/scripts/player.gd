c2bf03dc8440c01303b8973f74989b9b235bab28
SamuelMTL <samuel.mtl11@gmail.com>
2025-02-19 14:27:50 -0300

    dash adicionado

--- amazonproject/scripts/player.gd
+++ amazonproject/scripts/player.gd
@@ -8,6 +8,10 @@ var target: CombatEntity = null
 
 var last_direction = "down"
 
+const DASH_SPEED = 900
+var dashing = false
+var can_dash = true
+
 func _physics_process(delta):
 	handle_input()
 	update_animation()
@@ -15,12 +19,24 @@ func _physics_process(delta):
 	
 func handle_input():
 	var move_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	move_entity(move_direction)
-
+	
+	if Input.is_action_just_pressed("dash") and can_dash:
+		dashing = true
+		can_dash = false
+		$DashTimer.start()
+		$DashCooldown.start() 
+		
+	if dashing:
+		player_dash(move_direction)
+	else:
+		move_entity(move_direction)
+		
 	if Input.is_action_just_pressed("attack") and is_alive:
 		if enemy_in_range and is_instance_valid(target):
 			attack(target)
 
+func player_dash(direction: Vector2):
+	velocity = direction.normalized() * DASH_SPEED
 	
 func update_animation():
 	if is_attacking:
@@ -76,3 +92,11 @@ func on_death():
 	print("Player morreu!")
 	get_tree().change_scene_to_file("res://scenes/aldeia.tscn")
 	health = 100
+
+
+func _on_dash_timer_timeout() -> void:
+	dashing = false
+
+
+func _on_dash_cooldown_timeout() -> void:
+	can_dash = true
