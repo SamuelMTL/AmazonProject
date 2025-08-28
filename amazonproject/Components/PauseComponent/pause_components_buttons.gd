8fa4b645a898b3fb17043ddcccce6c0ab2b85f92
SamuelMTL <samuel.mtl11@gmail.com>
2025-08-28 13:38:06 -0300

    fluxo pronto

---
+++ amazonproject/Components/PauseComponent/pause_components_buttons.gd
@@ -0,0 +1,6 @@
+extends Button
+
+@export var label_text: String
+
+func _ready() -> void:
+	text = label_text
--- amazonproject/scenes/Dungeons/MataDaTerraFirme/dungeon.gd
+++ amazonproject/scenes/Dungeons/MataDaTerraFirme/dungeon.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
 @onready var camera = $Camera2D
+@onready var pause_menu = $PauseComponent
 @onready var player = $Player
+@onready var inventario = $Inventario
 
 var room_size = Vector2(640, 360)  # Ajuste para o tamanho real da sala
 
 func _process(delta):
-	update_camera_position()
+	update_position(camera)
+	update_position(pause_menu)
+	update_position(inventario)
 
-func update_camera_position():
+func update_position(entidade):
 	# Calcula a sala baseada na posição do jogador
 	var current_room_x = int(player.position.x / room_size.x)
 	var current_room_y = int(player.position.y / room_size.y)
@@ -17,4 +21,6 @@ func update_camera_position():
 	var target_position = Vector2((current_room_x + 0.5) * room_size.x,(current_room_y + 0.5) * room_size.y)
 	
 	# Move a câmera suavemente para a nova posição da sala
-	camera.position = target_position
+	entidade.position = target_position
+	
+	
