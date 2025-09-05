689746537edf8a024eeff3542715874663673a0d
SamuelMTL <samuel.mtl11@gmail.com>
2025-09-05 17:42:26 -0300

    matintashop

--- amazonproject/Components/MatintaShopComponent/matinta_shop.gd
+++ amazonproject/Components/MatintaShopComponent/matinta_shop.gd
@@ -1,1 +1,65 @@
 extends Control
+
+var collectibles_prices = {
+	"Resina Ancestral": 2,
+	"Fragmentos de Cipó-Sagrado": 3,
+	"Cristais de Seiva": 10,
+	"Flores da Lua": 7,
+	"Cinzas do Curupira": 8,
+	"Penas do Uirapuru": 12,
+	"Orvalho de Jiboia Encantada": 6,
+	"Raiz de Iara": 9,
+	"Luzes de Fogo-Fatuo": 4,
+	"Areia do Tempo Parado": 5,
+	"Lagrimas de Boto": 11,
+	"Petalas da Vitoria-Regia": 7,
+	"Esporos de Fungos-Ancestrais": 3,
+	"Sementes do Pequi-Espiritual": 6,
+	"Poeira de Ossos de Mapinguari": 14
+}
+
+var weapons_prices = {
+	"Lança de Madeira Petrificada": 50,
+	"Boleadeira de Cipó": 50,
+	"Zarabatana Espiritual": 40
+}
+
+var armors_prices = {
+	"Capa de Folhagem Viva": 20,
+	"Peitoral de Casca de Samaúma": 30,
+	"Botas de Borracha Natural": 20
+}
+
+func sell_collectible(item_name: String, amount: int):
+	if not PlayerInventory.collectibles.has(item_name):
+		return false
+	
+	if PlayerInventory.collectibles[item_name] < amount:
+		return false
+	
+	# Calcula preço
+	var total_price = collectibles_prices.get(item_name, 0) * amount
+	
+	PlayerInventory.remove_collectible(item_name, amount)
+	PlayerInventory.add_coins(total_price)
+	return true
+	
+func buy_weapon(weapon_name: String):
+	var price = weapons_prices.get(weapon_name, -1)
+	if price == -1:
+		return false
+	
+	if PlayerInventory.remove_coins(price):
+		PlayerInventory.add_weapon(weapon_name)
+		return true
+	return false
+	
+func buy_armor(armor_name: String):
+	var price = armors_prices.get(armor_name, -1)
+	if price == -1:
+		return false
+	
+	if PlayerInventory.remove_coins(price):
+		PlayerInventory.add_armor(armor_name)
+		return true
+	return false
