3562ff52132a812d26f9a60fd65e420b5fdfa6cf
SamuelMTL <samuel.mtl11@gmail.com>
2025-10-17 15:08:08 -0300

    cutscene

---
+++ amazonproject/Scenes/CutsceneScene/cutscene_scene.gd
@@ -0,0 +1,10 @@
+extends Control
+
+@onready var anim_player = $AnimationPlayer
+
+func _ready() -> void:
+	anim_player.play("cutscene1")
+	anim_player.animation_finished.connect(_on_animation_finished)
+
+func _on_animation_finished(anim_name):
+	get_tree().change_scene_to_file("res://Scenes/aldeia.tscn")
