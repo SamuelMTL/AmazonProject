939e08b2af35463c03825332264620a5859ed0e8
SamuelMTL <samuel.mtl11@gmail.com>
2025-11-24 15:59:40 -0300

    Update boss_fire_attack.gd

--- amazonproject/Components/BossAttacks/FireAttack/boss_fire_attack.gd
+++ amazonproject/Components/BossAttacks/FireAttack/boss_fire_attack.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
 @export var lifetime := 4.0  # segundos até desaparecer
+@onready var animation_player = $AnimationPlayer
 
 func _ready():
 	await get_tree().create_timer(lifetime).timeout
+	animation_player.play("end_animation")
+	await animation_player.animation_finished
+
 	queue_free()
