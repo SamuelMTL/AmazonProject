352f4c1dc61838db54f5cd5a00ec10edfa474b62
SamuelMTL <samuel.mtl11@gmail.com>
2025-11-25 16:31:37 -0300

    sound attack criado e ciclo de ataques criado

--- amazonproject/Components/BossAttacks/SoundAttack/sound_attack.gd
+++ amazonproject/Components/BossAttacks/SoundAttack/sound_attack.gd
@@ -1,1 +1,12 @@
 extends Node2D
+
+@export var speed: float = 250.0
+@export var direction: Vector2 = Vector2.DOWN
+@export var lifetime: float = 4.0
+
+func _ready():
+	await get_tree().create_timer(lifetime).timeout
+	queue_free()
+	
+func _physics_process(delta: float) -> void:
+	position += direction * speed * delta
