604cfa7b980ef393cd55c1f33e60d057a21fa03c
SamuelMTL <samuel.mtl11@gmail.com>
2025-11-26 11:38:56 -0300

    a lança ta dando dano agr

--- amazonproject/Components/BossAttacks/LancaAttack/boss_lanca.gd
+++ amazonproject/Components/BossAttacks/LancaAttack/boss_lanca.gd
@@ -10,7 +10,8 @@ func _process(delta: float) -> void:
 	position += direction * speed * delta
 	rotation = direction.angle() + deg_to_rad(90)
 
-func _on_body_entered(body: Node2D) -> void:
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		body.take_damage(damage)
 		queue_free()
