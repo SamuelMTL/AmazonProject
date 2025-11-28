6b98610c014db4b887b2aa7d9785b7658aeb95c0
Efraim <efra10_efraim@hotmail.com>
2025-11-28 10:00:23 -0300

    Sprites dos coletáveis adicionados.

--- amazonproject/item.gd
+++ amazonproject/item.gd
@@ -11,6 +11,17 @@ func _ready() -> void:
 	item_name = item_names.pick_random()
 	preco_item = PlayerInventory.collectibles[item_name]["preco"]
 	print("Item dropado: ", item_name, ", ", preco_item)
+	# Qual sprite mostrar:
+	if item_name == "Flores da Lua":
+		get_node("Area2D/FlorLua").visible = true
+		get_node("Area2D/ResinaAncestral").visible = false
+	elif item_name == "Resina Ancestral":
+		get_node("Area2D/FlorLua").visible = false
+		get_node("Area2D/ResinaAncestral").visible = true
+	else:
+		get_node("Area2D/FlorLua").visible = true
+		get_node("Area2D/ResinaAncestral").visible = false
+		get_node("Area2D/FlorLua").texture = load("res://assets/arvore.png")
  
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"): # opcional, se quiser garantir que só o player coleta
