fac59fe377c597bba185a3542d4641402710a5d9
Efraim <efra10_efraim@hotmail.com>
2025-11-28 08:42:35 -0300

    Drop aleatório dos itens (Resina Ancestral ou Flor da Lua).

--- amazonproject/item.gd
+++ amazonproject/item.gd
@@ -3,6 +3,14 @@ extends Node2D
 @export var item_name: String
 @export var preco_item: int = 0  # define o preço aqui
 
+# Lista de itens coletáveis
+@onready var item_names = PlayerInventory.collectibles.keys()
+
+func _ready() -> void:
+	# Selecionar aleatoriamente um item coletável
+	item_name = item_names.pick_random()
+	preco_item = PlayerInventory.collectibles[item_name]["preco"]
+	print("Item dropado: ", item_name, ", ", preco_item)
  
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"): # opcional, se quiser garantir que só o player coleta
